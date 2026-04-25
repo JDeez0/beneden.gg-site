7811712a5c9fe177a9eac74666a7a685aae4143e
Jasper Deen <jasperhdeen@gmail.com>
2026-04-25 12:54:22 -0600

    Copy CNAME file to output directory

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,8 @@ module.exports = function (eleventyConfig) {
     // Copy static assets to output
     eleventyConfig.addPassthroughCopy("./src/css");
     eleventyConfig.addPassthroughCopy("./src/img");
+    // Copy CNAME for custom domain
+    eleventyConfig.addPassthroughCopy("./CNAME");
 
     return {
         dir: {
